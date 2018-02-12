0b24541d51ec181c5818e3c0296edd6b78bb5f9a
Michael Vilabrera <m.vilabrera@gmail.com>
2018-02-11 19:45:35 -0500

    add Checklist object, ListDetailVC, project files & storyboard

---
+++ Checklists/Checklist.swift
@@ -0,0 +1,13 @@
+//
+//  Checklist.swift
+//  Checklists
+//
+//  Created by Michael Vilabrera on 2/11/18.
+//  Copyright © 2018 Michael Vilabrera. All rights reserved.
+//
+
+import UIKit
+
+class Checklist: NSObject {
+
+}
